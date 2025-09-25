7f54093602a009f3a140b4c3dfe7f9a80500618d
root <root@vanessa.localdomain>
2025-09-25 07:25:42 -0300

    fix: corrigir formatação do código.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-const CarrinhoCompras = require('./models/CarrinhoCompras.js')
+const CarrinhoCompras = require("./models/CarrinhoCompras.js");
 
-const meuCarrinho = new CarrinhoCompras()
+const meuCarrinho = new CarrinhoCompras();
 // Adicionar produtos ao carrinho
-meuCarrinho.adicionarProduto({ nome: 'Notebook', preco: '2000'})
-meuCarrinho.adicionarProduto({ nome: 'Notebook', preco: '2000'})
-meuCarrinho.adicionarProduto({ nome: 'Mouse', preco: '50'}, 3)
-
+meuCarrinho.adicionarProduto({ nome: "Notebook", preco: "2000" });
+meuCarrinho.adicionarProduto({ nome: "Notebook", preco: "2000" });
+meuCarrinho.adicionarProduto({ nome: "Mouse", preco: "50" }, 3);
 
 // Listar produtos
-console.log(meuCarrinho.listarProdutos())
+console.log(meuCarrinho.listarProdutos());
 
 // Calcular o total
-console.log('Total:', meuCarrinho.calcularTotal())
+console.log("Total:", meuCarrinho.calcularTotal());
 
 // console.log(meuCarrinho.removerProduto('NOTEBOOK'))
--- src/models/CarrinhoCompras.js
+++ src/models/CarrinhoCompras.js
@@ -1,45 +1,49 @@
 class CarrinhoCompras {
-    constructor() {
-        this.produtos = []
+  constructor() {
+    this.produtos = [];
+  }
+
+  adicionarProduto(produto, quantidade = 1) {
+    if (quantidade <= 0) {
+      console.log("A quantidade do produto precisa ser maior que zero.");
+      return;
     }
 
-    adicionarProduto(produto, quantidade = 1) {
-        if (quantidade <= 0) {
-            console.log("A quantidade do produto precisa ser maior que zero.")
-            return
-        }
+    const produtoExistente = this.produtos.find(
+      (p) => p.nome.trim().toLowerCase() === produto.nome.trim().toLowerCase()
+    );
 
-        const produtoExistente = this.produtos.find(
-            p => p.nome.trim().toLowerCase() === produto.nome.trim().toLowerCase()
-        )
-
-        if (produtoExistente) {
-            produtoExistente.quantidade += quantidade
-            console.log(`Adicionado mais ${quantidade} unidade(s) de "${produto.nome}". Quantidade total: ${produtoExistente.quantidade}.`)
-        } else {
-            this.produtos.push({...produto, quantidade: quantidade})
-            console.log(`${quantidade} unidade(s) de "${produto.nome}" adicionada(s) ao carrinho.`);
-        }
+    if (produtoExistente) {
+      produtoExistente.quantidade += quantidade;
+      console.log(
+        `Adicionado mais ${quantidade} unidade(s) de "${produto.nome}". Quantidade total: ${produtoExistente.quantidade}.`
+      );
+    } else {
+      this.produtos.push({ ...produto, quantidade: quantidade });
+      console.log(
+        `${quantidade} unidade(s) de "${produto.nome}" adicionada(s) ao carrinho.`
+      );
     }
+  }
 
-    listarProdutos() {
-        return this.produtos
-    }
+  listarProdutos() {
+    return this.produtos;
+  }
 
-    calcularTotal() {
-        const total = this.produtos.reduce((soma, produto) => {
-            return soma + parseFloat(produto.preco)
-        }, 0)
+  calcularTotal() {
+    const total = this.produtos.reduce((soma, produto) => {
+      return soma + parseFloat(produto.preco);
+    }, 0);
 
-        return total
+    return total;
+  }
 
-    }
-
-    removerProduto(nome) {
-        const listaAtualizada = this.produtos.filter(produto => produto.nome.toLowerCase() !== nome.toLowerCase())
-        return listaAtualizada
-    }
-
+  removerProduto(nome) {
+    const listaAtualizada = this.produtos.filter(
+      (produto) => produto.nome.toLowerCase() !== nome.toLowerCase()
+    );
+    return listaAtualizada;
+  }
 }
 
-module.exports = CarrinhoCompras
+module.exports = CarrinhoCompras;
